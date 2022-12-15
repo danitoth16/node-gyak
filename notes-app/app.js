7e01aa9a107fe2478cf0f22dc8d65395e75beaee
Toth Dani <dani.toth13@gmail.com>
2022-12-15 23:39:43 +0100

    note commands added /add/remove/list/read

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,49 @@
-const validator = require('validator')
 const getNotes = require('./notes.js')
+const yargs = require('yargs')
+const { command, demand, demandOption } = require('yargs')
 
-const message = getNotes()
-console.log(message)
 
-console.log(validator.isEmail('example.com'))
-console.log(validator.isURL('https://dani.com'))
+// Create add command
+yargs.command ({
+    command: 'add',
+    describe: 'Add a new note!',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true
+        }
+    },
+    handler: function(argv) {
+        console.log('Adding a new note!', argv)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command:'remove',
+    describe: 'Remove a note!',
+    handler: function() {
+        console.log('Remove a note!')
+    }
+})
+
+// Create read command
+yargs.command({
+    command:'read',
+    describe: 'Read a note!',
+    handler: function() {
+        console.log('Reading a note!')
+    }
+})
+
+// Create list command
+yargs.command({
+    command:'list',
+    describe: 'List the notes!',
+    handler: function() {
+        console.log('Listing the notes!')
+    }
+})
+
+
+console.log(yargs.argv)
